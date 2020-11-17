ca5f74825f6e1aa5e784d1db5c341ea3bac98332
xilareyna <68404722+xilareyna@users.noreply.github.com>
2020-11-17 10:11:19 -0600

    uploading design

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("yes im connected");
+const getdivs = document.querySelector(".container");
+const modal = document.querySelector(".modal");
+// const closingX = document.querySelector(".modal-close");
+
+// tried to close modal
+// closingX.addEventListener("click", () => {
+//     modal.classList.add("closed");
+// });
+
+for (let i = 0; i <= 42; i++) {
+  let newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "disc");
+  getdivs.appendChild(newDiv);
+}
+
+for (let i = 0; i <= 7; i++) {
+  let newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "permDisc");
+  getdivs.appendChild(newDiv);
+}
+
+// for (let i = 0; i <= 6; i++) {
+//   let newDiv = document.createElement("div");
+//   newDiv.setAttribute("class", "disc");
+//   getdivs.appendChild(newDiv);
+// }
