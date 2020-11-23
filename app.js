afdadd476ba212f2d2da1724ecb9066e82c78a7f
xilareyna <68404722+xilareyna@users.noreply.github.com>
2020-11-22 23:32:09 -0600

    uploading final game touches

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const getdivs = document.querySelector(".container");
 const modal = document.querySelector(".modal");
 const closingX = document.querySelector(".modal-close");
-const h3 = document.querySelector("h3");
+const h3 = document.querySelector(".playNow");
+
+// object that stores players and
+//SwitchPlayer & whose turn Functions
 const players = {
   player1: true,
   player2: false,
@@ -43,52 +46,43 @@ for (let c = 0; c < 6; c++) {
   for (let r = 0; r < 7; r++) {
     let makeRow = document.createElement("div");
     makeRow.className = "row";
-
     let id = "c" + c + "r" + r;
-
     makeRow.id = id;
-
     addCol.appendChild(makeRow);
 
     let getCols = document.getElementsByClassName("col");
-    let getRows = document.getElementsByClassName("row");
+    // let getRows = document.getElementsByClassName("row");
     let boardHeight = getCols.length;
-    console.log(boardHeight);
-    //    document.getElementById(id).addEventListener("click", function (e) {
-    //       changeCSS(e.target.id);
-    //     });
 
-    // added event listener here because of what I had before and added
-    //to the event with code inspo that arthur sent
-
+    // event listener that takes in unique id
     document.getElementById(id).addEventListener("click", function (e) {
       let target = e.currentTarget;
-
-      let rows = document.querySelectorAll(".row");
-
+      //loops through the columns backwards
       for (let rowz = boardHeight - 1; rowz >= 0; rowz--) {
         let rowChild = getCols[rowz].children;
-        // console.log(rowChild);
+        //loops through the children of the columns
         for (let col = 0; col < rowChild.length; col++) {
           console.log(rowChild[col].getAttribute("id"));
           if (rowChild[col].getAttribute("id") === target.getAttribute("id")) {
-            // if (rowChild[col].style.backgroundColor === "white") {
+            //checks if the column ids are = to the target attribute id
             if (players.whoseTurn().player === "p1") {
-              // rowChild[col].style.backgroundColor = "#8CCFF3";
-              console.log("im running");
               const bottomId = "#c" + c + "r" + 6;
-              changeCellColor(bottomId, 6, "blue");
+              changeCellColor(bottomId, 6, "#d0b3f2");
+              //new
+              rowChild[col].classList.add("#d0b3f2");
               checkForWins();
 
               return;
             } else {
               const bottomId = "#c" + c + "r" + 6;
-              changeCellColor(bottomId, 6, "purple");
+              changeCellColor(bottomId, 6, "#BBF4F3");
+              //new
+              rowChild[col].classList.add("#BBF4F3");
+
               checkForWins();
 
               return;
             }
-            // }
           }
         }
         function changeCellColor(id, rowNumber, color) {
@@ -98,7 +92,6 @@ for (let c = 0; c < 6; c++) {
               document
                 .querySelector(id)
                 .setAttribute("data-taken", players.whoseTurn().player);
-              console.log(document.querySelector(id));
             } else {
               for (let i = rowNumber - 1; i > 0; i--) {
                 if (checkOtherCells(`#c${c}r${i}`)) {
@@ -112,18 +105,13 @@ for (let c = 0; c < 6; c++) {
               document
                 .querySelector(id)
                 .setAttribute("data-taken", players.whoseTurn().player);
-              console.log(document.querySelector(id));
             }
-            // document.getElementById("c0").style.backgroundColor = "green";
-          } else {
-            h3.innerHTML = "its player 1 turn";
-            document.querySelector(id).style.backgroundColor = "#B292F5";
           }
           players.switchPlayer();
           h3.innerHTML =
             players.whoseTurn().player === "p1"
-              ? "its player 1 turn"
-              : "its player 2 turn";
+              ? "Player 1, it's YOUR turn!🦄"
+              : "Player 2, it's YOUR turn!🦋";
         }
 
         function checkOtherCells(id) {
@@ -136,248 +124,51 @@ for (let c = 0; c < 6; c++) {
 
 let player1 = true;
 
-function checkForWins() {
-  for (let i = 0; i < rows.length; i++) {
-    let currentRow = rows[i].children;
-
-    for (let x = 0; x < 4; x++) {
-      let currentCell = currentRow[x];
-
-      console.log(currentCell.style.backgroundColor);
-      if (
-        currentCell.style.backgroundColor !== "white" &&
-        currentCell.style.backgroundColor ===
-          currentRow[x + 1].style.backgroundColor &&
-        currentCell.style.backgroundColor ===
-          currentRow[x + 2].style.backgroundColor &&
-        currentCell.style.backgroundColor ===
-          currentRow[x + 3].style.backgroundColor
-      ) {
-        alert(player1 + " Wins!");
-      }
-    }
-  }
-}
-
-// if (
-//     !document.getElementById(colNum, rowNum).classList.contains("taken")
-// ) {
-//     document.getElementById(colNum, rowNum).setAttribute("class", "taken");
-
-//     changeCellColor(colNum, rowNum);
-// } else if (
-//     !document.getElementById(colNum, rowNum).classList.contains("taken")
-// ) {
-//     changeCellColor(colNum, rowNum);
-// }
-//   arr[i] = player1;
-//   const colNum = Number(e.target.id[1]);
-//   const rowNum = Number(e.target.id[3]);
-
-//   document.getElementById(addCo.id).addEventListener("click", function (e) {
-//     //   makeCol.setAttribute("class", "taken");
-//     //   arr[i] = player1;
-
-//     changeCSS(e.target.id);
-//   });
-//   arr[a] = ["c" + a];
-
-// let activities = [
-//   ["Work", 9],
-//   ["Eat", 1],
-//   ["Commute", 2],
-//   ["Play Game", 1],
-//   ["Sleep", 7],
+// winningComb1 = [
+//   [c0r6, c1r6, c2r6, c3r6],
+//   [c1r6, c2r6, c3r6, c4r6],
+//   [c2r6, c3r6, c4r6, c5r6],
 // ];
-// console.table(activities);
+// (winningComb2 = [c0r5, c1r5, c2r5, c3r5]),
+//   [(c1r5, c2r5, c3r5, c4r5)],
+//   [(c2r5, c3r5, c4r5, c5r5)];
+// (winningComb3 = [c0r4, c1r4, c2r4, c3r4]),
+//   [(c1r4, c2r4, c3r4, c4r4)],
+//   [(c2r4, c3r4, c4r4, c5r4)];
+// (winningComb4 = [c0r3, c1r3, c2r3, c3r3]),
+//   [(c1r3, c2r3, c3r3, c4r3)],
+//   [(c2r3, c3r3, c4r3, c5r3)];
+// (winningComb5 = [c0r2, c1r2, c2r2, c3r2]),
+//   [(c1r2, c2r2, c3r2, c4r2)],
+//   [(c2r2, c3r2, c4r2, c5r2)];
+// (winningComb6 = [c0r1, c1r1, c2r1, c3r1]),
+//   [(c1r1, c2r1, c3r1, c4r1)],
+//   [(c2r1, c3r1, c4r1, c5r1)];
 
-// const getIDZ = document.querySelectorAll(".taken");
-// if (getIDZ === true) {
-//   alert("that cell is taken");
-// }
-// console.log(getIDZ);
-
-// for (let i = 0; i < 6; i++) {
-//   let addCol = document.createElement("div");
-//   addCol.className = "col";
-//   addCol.id = "c" + i;
-//   getdivs.appendChild(addCol);
-// }
-// document.getElementById("c5r0").onclick = changeColor;
-
-//
-// for (let k=0; )
-
-// getCol0.addEventListener("click", () => {
-//   if (player1 === true) {
-//     h3.innerHTML = "its player 1 turn";
-//   } else {
-//     h3.innerHTML = "its player 2 turn";
-//   }
-//   player1 = !player1;
-// });
-
-// getCol1.addEventListener("click", () => {
-//   if (player1 === true) {
-//     h3.innerHTML = "its player 1 turn";
-//   } else {
-//     h3.innerHTML = "its player 2 turn";
-//   }
-//   player1 = !player1;
-// });
-
-// getCol2.addEventListener("click", () => {
-//   if (player1 === true) {
-//     h3.innerHTML = "its player 1 turn";
-//   } else {
-//     h3.innerHTML = "its player 2 turn";
-//   }
-//   player1 = !player1;
-// });
-// getCol3.addEventListener("click", () => {
-//   if (player1 === true) {
-//     h3.innerHTML = "its player 1 turn";
-//   } else {
-//     h3.innerHTML = "its player 2 turn";
-//   }
-//   player1 = !player1;
-// });
-// getCol4.addEventListener("click", () => {
-//   if (player1 === true) {
-//     h3.innerHTML = "its player 1 turn";
-//   } else {
-//     h3.innerHTML = "its player 2 turn";
-//   }
-//   player1 = !player1;
-// });
-// getCol5.addEventListener("click", () => {
-//   if (player1 === true) {
-//     h3.innerHTML = "its player 1 turn";
-//   } else {
-//     h3.innerHTML = "its player 2 turn";
-//   }
-//   player1 = !player1;
-// });
-
-// const getCol0 = document.querySelector("#c0");
-// const getCol1 = document.querySelector("#c1");
-// const getCol2 = document.querySelector("#c2");
-// const getCol3 = document.querySelector("#c3");
-// const getCol4 = document.querySelector("#c4");
-// const getCol5 = document.querySelector("#c5");
-
-//
-// for (let i = 0; i < 7; i++) {
-//   let makeCol = document.createElement("div");
-//   makeCol.className = "cols";
-//   makeCol.id = "c1r" + i;
-//   getCol1.appendChild(makeCol);
+// function checkForWins(id) {
+//   winningComb1.array.forEach((element) => {});
 // }
 
-// for (let i = 0; i < 7; i++) {
-//   let makeCol = document.createElement("div");
-//   makeCol.className = "cols";
-//   makeCol.id = "c2r" + i;
-//   getCol2.appendChild(makeCol);
-// }
-// for (let i = 0; i < 7; i++) {
-//   let makeCol = document.createElement("div");
-//   makeCol.className = "cols";
-//   makeCol.id = "c3r" + i;
-//   getCol3.appendChild(makeCol);
-// }
-// for (let i = 0; i < 7; i++) {
-//   let makeCol = document.createElement("div");
-//   makeCol.className = "cols";
-//   makeCol.id = "c4r" + i;
-//   getCol4.appendChild(makeCol);
-// }
-// for (let i = 0; i < 7; i++) {
-//   let makeCol = document.createElement("div");
-//   makeCol.className = "cols";
-//   makeCol.id = "c5r" + i;
-//   getCol5.appendChild(makeCol);
-// }
+// check through column
+//is this p1 using "data taken" & whose turn
+// are they ===
+// dont assign to background color
 
-// function myFunction() {
-//   document.querySelectorAll(".cols").style.color = "red";
-// }
-// const getColly = document.querySelector(".cols");
-// // console.log(getColly);
-// getColly.addEventListener("click", () => {
-//   getColly.classList.add("style");
-// });
-// const clickMe = (clr) => {
-//   getCol0.classList.add("red");
-// };
-// // const getCol6 = document.querySelector("#r6");
-// getCol0.addEventListener("click", clickMe);
-// getRow1.addEventListener("click", clickMe);
-// getRow2.addEventListener("click", clickMe);
-// getRow3.addEventListener("click", clickMe);
-// getRow4.addEventListener("click", clickMe);
-// getRow5.addEventListener("click", clickMe);
-// getRow6.addEventListener("click", clickMe);
-
-// const clickMeAgain = () => {
-//   getCol1.classList.add("red");
-// };
-// const getCol1 = document.querySelector(".row");
-// getCol1.addEventListener("click", clickMeAgain);
-
-// const getCol = document.querySelector(".column");
-// const getcol1 = document.getElementsByClassName("col-1");
-
-// for (let i = 0; i < 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "col1");
-//   getcol1.appendChild(newDiv);
-// }
-// getdivs.appendChild(getcol1);
-// for (let i = 0; i < 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "makeCol");
-//   newDiv.setAttribute("class", "col1");
-
-//   getCol.appendChild(newDiv);
-// }
-// for (let i = 0; i < 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "makeCol");
-
-//   newDiv.setAttribute("class", "col2");
-//   getCol.appendChild(newDiv);
-// }
-// for (let i = 0; i < 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "makeCol");
-
-//   newDiv.setAttribute("class", "col3");
-//   getCol.appendChild(newDiv);
-// }
-// for (let i = 0; i <= 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "col4");
-//   getCol.appendChild(newDiv);
-// }
-// for (let i = 0; i <= 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "col5");
-//   getCol.appendChild(newDiv);
-// }
-// for (let i = 0; i <= 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "col6");
-//   getCol.appendChild(newDiv);
-// }
-// for (let i = 0; i <= 6; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "col7");
-//   getCol.appendChild(newDiv);
-// }
-
-// for (let i = 0; i <= 7; i++) {
-//   let newDiv = document.createElement("div");
-//   newDiv.setAttribute("class", "permDisc");
-//   getdivs.appendChild(newDiv);
-// }
+// for (let i = 0; i < getCols.length; i++) {
+//     let currentRow = rows[i].children;
+//     for (let x = 0; x < currentRow.length; x++) {
+//         if (document.body.classList.contains()) {
+//             let currentCell = currentRow[x];
+//             if (document.body.classList.contains()) (
+//                 currentCell.style.backgroundColor !== "white" &&
+//                 currentCell.style.backgroundColor ===
+//                 currentRow[x + 1].style.backgroundColor &&
+//                 currentCell.style.backgroundColor ===
+//                 currentRow[x + 2].style.backgroundColor &&
+//                 currentCell.style.backgroundColor ===
+//                 currentRow[x + 3].style.backgroundColor
+//             ); {
+//                 alert(player1 + " Wins!");
+//             }
+//         }
+//     }
---
+++ markdown.md
@@ -0,0 +1,13 @@
+Connect 4
+
+Connect 4: is a two-player connection board game, in which the players choose a color and then take turns dropping colored discs into a six-column, seven-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal line of four of one's own discs.
+
+Technologies Used:
+
+JavaScript:Dom, Objects, functions, for loops, event listeners, conditionals
+Html
+CSS: Flexbox, modals , gif, imported fonts
+
+Lessons/Ongoing Issues:
+
+Struggled to get the win statement to work for hours using different approaches, hope to be able to finish it later on.
